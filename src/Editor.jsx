be64240b602adb38ffc402c9019395405741a2ff
greg <grzegorz.smolin@agricircle.com>
2023-07-02 14:34:19 -0400

    New editor actions - New character, new world

--- src/Editor.jsx
+++ src/Editor.jsx
@@ -5,16 +5,17 @@ import styles from './Editor.module.css';
 export const Editor = () => {
 	const [editor, setEditor] = useState(null);
 	const monacoEl = useRef(null);
-  const editorConfig = {
-		value: 'test',
-		automaticLayout: true,
-		language: 'plaintext',
-		theme: 'vs-dark',
-		minimap: { enabled: false },
-		contextmenu: true
-	};
 
 	useEffect(() => {
+    const editorConfig = {
+      value: 'test',
+      automaticLayout: true,
+      language: 'plaintext',
+      theme: 'vs-dark',
+      minimap: { enabled: false },
+      contextmenu: true
+    };
+
 		if (monacoEl) {
 			setEditor((editor) => {
 				if (editor) return editor;
@@ -24,7 +25,48 @@ export const Editor = () => {
 		}
 
 		return () => editor?.dispose();
-	}, [monacoEl.current, editor]);
+	}, [monacoEl, editor]);
+
+  useEffect(() => {
+    if (editor) {
+      editor.addAction({
+        id: "create-new-character",
+        label: "Create New Character",
+        keybindings: [
+          monaco.KeyMod.CtrlCmd | monaco.KeyMod.Shift | monaco.KeyCode.KeyC
+        ],
+        precondition: null,
+        keybindingContext: null,
+        contextMenuGroupId: "navigation",
+        contextMenuOrder: 1,
+        run: () => {
+          const currentSelection =
+          editor
+              .getModel()
+              .getValueInRange(editor.getSelection());
+          console.log(currentSelection);
+        },
+      })
+      editor.addAction({
+        id: "create-new-world",
+        label: "Create New World",
+        keybindings: [
+          monaco.KeyMod.CtrlCmd | monaco.KeyMod.Shift | monaco.KeyCode.KeyW
+        ],
+        precondition: null,
+        keybindingContext: null,
+        contextMenuGroupId: "navigation",
+        contextMenuOrder: 2,
+        run: () => {
+          const currentSelection =
+          editor
+              .getModel()
+              .getValueInRange(editor.getSelection());
+          console.log(currentSelection);
+        },
+      })
+    }
+  }, [editor])
 
 	return <div className={styles.Editor} ref={monacoEl}></div>;
 };
