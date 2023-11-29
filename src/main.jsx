36e4cc0ffbb23f19eecf5f766e84d90dc9531976
TheTranceMachine <grzegorz.smolin2@gmail.com>
2023-11-29 16:40:10 -0500

    chore - installed Bootswatch sandstone theme

--- src/main.jsx
+++ src/main.jsx
@@ -3,12 +3,13 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { RouterProvider } from "react-router-dom";
-import "bootstrap/dist/css/bootstrap.min.css";
+import 'bootswatch/dist/sandstone/bootstrap.min.css'; // Using bootswatch theme
+// import "bootstrap/dist/css/bootstrap.min.css";
 import "split-pane-react/esm/themes/default.css";
 import { AuthProvider } from "./contexts/AuthContext.jsx";
 import { router } from "../routes/index.jsx";
 import "./index.scss";
-import "./userWorkers.js";
+import "./userWorkers.js"; 
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
